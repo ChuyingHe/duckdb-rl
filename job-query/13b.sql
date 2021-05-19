ff51881a2dd236b1826d293d4bae60042750dced
ChuyingHe <heidihcy@outlook.com>
2021-05-19 10:48:23 +0200

    test

--- job-query/13b.sql
+++ job-query/13b.sql
@@ -1,24 +1,4 @@
-SELECT MIN(cn.name) AS producing_company,
-       MIN(miidx.info) AS rating,
-       MIN(t.title) AS movie_about_winning
-FROM company_name AS cn,
-     company_type AS ct,
-     info_type AS it,
-     info_type AS it2,
-     kind_type AS kt,
-     movie_companies AS mc,
-     movie_info AS mi,
-     movie_info_idx AS miidx,
-     title AS t
+SELECT MIN(cn.name) AS producing_company
+FROM company_name AS cn, movie_companies AS mc
 WHERE cn.country_code ='[us]'
-  AND ct.kind ='production companies'
-  AND it.info ='rating'
-  AND it2.info ='release dates'
-  AND kt.kind ='movie'
-  AND t.title != ''
-  AND (t.title LIKE '%Champion%'
-       OR t.title LIKE '%Loser%')
-  AND mi.movie_id = t.id
-  AND it2.id = mi.info_type_id
-  AND kt.id = t.kind_id;
-
+AND cn.id = mc.company_id;
